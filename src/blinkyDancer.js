ab089806dd3ce15024ad2dac8700d115d8ce9a7e
jachiu555 <jachiu555@gmail.com>
2019-09-10 12:35:22 -0700

    Converted to pseudoclassical and called the makeBlinkyDancer function to reference parent class's prototype method.

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -1,25 +1,21 @@
-var makeBlinkyDancer = function(top, left, timeBetweenSteps) {
-  makeDancer.call(this, top, left, timeBetweenSteps);
-
-  // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
-  // so we must keep a copy of the old version of this function
-
-  // var oldStep = blinkyDancer.step;
+var makeBlinkyDancer = function(top, left, timeBetweenSteps) {                                                         // child dancer that takes in the same parameters as the parent
+  makeDancer.call(this, top, left, timeBetweenSteps);                                                                  // need to call the dancer superclass (parent) with all their properties passed in aka this, as well as the parameters that are passed in
 };
-  // blinkyDancer.step = function() {
-    // call the old version of step at the beginning of any call to this new version of step
-    // oldStep();
-    // toggle() is a jQuery method to show/hide the <span> tag.
-    // See http://api.jquery.com/category/effects/ for this and
-    // other effects you can use on a jQuery-wrapped html tag.
-    makeBlinkyDancer.prototype = Object.create(makeDancer.prototype);
-    makeBlinkyDancer.prototype.constructor = makeBlinkyDancer;
 
-    makeBlinkyDancer.prototype.step = function(){
+// blinkyDancer.step = function() {
+// call the old version of step at the beginning of any call to this new version of step
+// oldStep();
+// toggle() is a jQuery method to show/hide the <span> tag.
+// See http://api.jquery.com/category/effects/ for this and
+// other effects you can use on a jQuery-wrapped html tag.
 
-      makeDancer.prototype.step.call(this);
+makeBlinkyDancer.prototype = Object.create(makeDancer.prototype);                                       // Object.create creates a new object in the makeBlinkyDancer.prototype and linking the parent's prototypes/methods into it
+makeBlinkyDancer.prototype.constructor = makeBlinkyDancer;                                              // then, in order for makeBlinkyDancer object to have it's own
 
-      this.$node.toggle();
+makeBlinkyDancer.prototype.step = function() {
 
-  };
-  
+  makeDancer.prototype.step.call(this);
+
+  this.$node.toggle();
+
+};
